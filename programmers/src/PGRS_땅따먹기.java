ac21179a9e240039f03e2c67dd80c2abc883f36b
seonrizee <seonrizee@gmail.com>
2025-07-06 21:19:31 +0900

    ✅ refactor: 프로그래머스 - 땅따먹기

--- programmers/src/PGRS_땅따먹기.java
+++ programmers/src/PGRS_땅따먹기.java
@@ -7,21 +7,23 @@ public class PGRS_땅따먹기 {
 
     int R = land.length;
     int C = 4;
-    int[][] sum = new int[R][C];
+    int[][] dp = new int[R][C];
 
-    sum[0] = Arrays.copyOf(land[0], 4);
+    dp[0] = Arrays.copyOf(land[0], 4);
 
     for (int r = 1; r < R; r++) {
-      sum[r][0] = land[r][0] + Math.max(sum[r - 1][1], Math.max(sum[r - 1][2], sum[r - 1][3]));
-      sum[r][1] = land[r][1] + Math.max(sum[r - 1][0], Math.max(sum[r - 1][2], sum[r - 1][3]));
-      sum[r][2] = land[r][2] + Math.max(sum[r - 1][0], Math.max(sum[r - 1][1], sum[r - 1][3]));
-      sum[r][3] = land[r][3] + Math.max(sum[r - 1][0], Math.max(sum[r - 1][1], sum[r - 1][2]));
+      for (int c = 0; c < C; c++) {
+        int max = 0;
+        for (int k = 0; k < C; k++) {
+          if (k != c) {
+            max = Math.max(max, dp[r - 1][k]);
+          }
+          dp[r][c] = land[r][c] + max;
+        }
+      }
     }
 
-    answer =
-        Math.max(sum[R - 1][0], Math.max(sum[R - 1][1], Math.max(sum[R - 1][2], sum[R - 1][3])));
-
-    return answer;
+    return Arrays.stream(dp[R - 1]).max().getAsInt();
   }
 }
 
