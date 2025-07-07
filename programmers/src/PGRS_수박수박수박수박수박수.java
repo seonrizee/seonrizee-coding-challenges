96596a1190c4f5bd1b848d0ce425344392e7c24c
seonrizee <seonrizee@gmail.com>
2025-07-08 02:07:56 +0900

    ✅ solve: 프로그래머스 - 수박수박수박수박수박수?

---
+++ programmers/src/PGRS_수박수박수박수박수박수.java
@@ -0,0 +1,9 @@
+public class PGRS_수박수박수박수박수박수 {
+    public String solution(int n) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            sb.append(i % 2 == 0 ? "수" : "박");
+        }
+        return sb.toString();
+    }
+}
