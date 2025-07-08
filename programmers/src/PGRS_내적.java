e4737f3fe19b9b79cef64e52a67c069627b7d4c5
seonrizee <seonrizee@gmail.com>
2025-07-08 09:08:23 +0900

    ✅ refactor: 프로그래머스 - 내적

--- programmers/src/PGRS_내적.java
+++ programmers/src/PGRS_내적.java
@@ -1,13 +1,9 @@
+import java.util.stream.IntStream;
+
 public class PGRS_내적 {
     public int solution(int[] a, int[] b) {
-        int answer = 0;
-
-        int n = a.length;
-
-        for (int k = 0; k < n; k++) {
-            answer += a[k] * b[k];
-        }
-
-        return answer;
+        return IntStream.range(0, a.length)
+                .map(i -> a[i] * b[i])
+                .sum();
     }
 }
