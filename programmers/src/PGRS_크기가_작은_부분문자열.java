a3bf7f1f8298131887b7668e095156bacef1608f
seonrizee <seonrizee@gmail.com>
2025-07-15 19:50:54 +0900

    ✨ refactor: 프로그래머스 - 크기가 작은 부분문자열

--- programmers/src/PGRS_크기가_작은_부분문자열.java
+++ programmers/src/PGRS_크기가_작은_부분문자열.java
@@ -1,20 +1,17 @@
+import java.util.stream.IntStream;
+
 public class PGRS_크기가_작은_부분문자열 {
 
     public int solution(String t, String p) {
 
-        int answer = 0;
         int tLength = t.length();
         int pLength = p.length();
 
         long pValue = Long.parseLong(p);
-        for (int idx = 0; idx <= tLength - pLength; idx++) {
-            long tCurValue = Long.parseLong(t.substring(idx, idx + pLength));
-            if (pValue >= tCurValue) {
-                answer++;
-            }
-        }
-
-        return answer;
+        return (int) IntStream.rangeClosed(0, tLength - pLength)
+                .mapToLong(idx -> Long.parseLong(t.substring(idx, idx + pLength)))
+                .filter(tValue -> tValue <= pValue)
+                .count();
     }
 
 }
