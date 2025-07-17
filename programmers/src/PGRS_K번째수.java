8858f4d5ca768fd80d79947ea9a127864c14756c
seonrizee <seonrizee@gmail.com>
2025-07-17 21:44:39 +0900

    refactor: 프로그래머스 - K번째수

--- programmers/src/PGRS_K번째수.java
+++ programmers/src/PGRS_K번째수.java
@@ -1,22 +1,13 @@
 import java.util.Arrays;
 
-public class PGRS_K번째수 {
-
+class PGRS_K번째수 {
     public int[] solution(int[] array, int[][] commands) {
-
-        int[] answer = new int[commands.length];
-        int idx = 0;
-        for (int[] command : commands) {
-            int stIdx = command[0] - 1;
-            int edIdx = command[1];
-            int K = command[2];
-
-            int[] subArray = Arrays.copyOfRange(array, stIdx, edIdx);
-            Arrays.sort(subArray);
-
-            answer[idx++] = subArray[K - 1];
-        }
-
-        return answer;
+        return Arrays.stream(commands)
+                .mapToInt(command -> {
+                    int[] sliced = Arrays.copyOfRange(array, command[0] - 1, command[1]);
+                    Arrays.sort(sliced);
+                    return sliced[command[2] - 1];
+                })
+                .toArray();
     }
 }
